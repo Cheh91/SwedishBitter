7f01ca4bf5c97dd09f62451d743cf5d199585867
Cheh91 <4eh91@ukr.net>
2019-12-01 21:01:34 +0200

    added reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,25 @@
-$(function(){
+$(function () {
 
-    $('.list__wrapper .tab').on('click', function(event) {
+    $('.list__wrapper .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-            $('.list__wrapper').find('.tab-item').removeClass('active-tab').hide();
-            $('.list__wrapper .tabs').find('.tab').removeClass('active');
-            $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
-            return false;
-        });
+        $('.list__wrapper').find('.tab-item').removeClass('active-tab').hide();
+        $('.list__wrapper .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
+    $('.wrapper__reviews .tab__reviews').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.wrapper__reviews').find('.tab-page').removeClass('active-tab').hide();
+        $('.wrapper__reviews .tabs__reviews').find('.tab__reviews').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
 
 
 });
